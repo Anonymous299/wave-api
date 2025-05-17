a7547a3d566c90b2617bccf662621404942b8423
ryan <ryanennns@gmail.com>
2025-05-16 20:10:07 -0400

    health endpoint

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('health', function () {
+    return response()->json(['status' => 'ok']);
+})->name('health');
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::prefix('users')->name('users.')->group(function () {
         Route::get('nearby', GetNearbyUsers::class)->name('nearby');
