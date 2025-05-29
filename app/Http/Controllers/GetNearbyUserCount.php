f404ca89e7582a371bb8b0ddc0f660e9c963f5aa
Ryan Enns <ryanennns@gmail.com>
2025-05-29 14:10:58 -0400

    add nearby user count endpoint

---
+++ app/Http/Controllers/GetNearbyUserCount.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Clickbar\Magellan\Database\PostgisFunctions\ST;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class GetNearbyUserCount extends Controller
+{
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     *
+     * Get Nearby User Count
+     *
+     * Retrieves the count of users within a specified distance from the authenticated user's location.
+     *
+     * @queryParam distance float required The distance in kilometers to search for users.
+     *
+     * @response 200 {
+     *  "count": 5,
+     *  "distance": 50,
+     *  "origin": {
+     *    "latitude": 43.4779751,
+     *    "longitude": -80.5197298
+     *  }
+     */
+    public function __invoke(Request $request): JsonResponse
+    {
+        $request->validate(['distance' => 'required|numeric|min:1|max:1000']);
+
+        /** @var User $authUser */
+        $authUser = auth()->user();
+
+        $distanceInKm = $request->input('distance');
+        $origin = $authUser->location;
+
+        $count = User::query()
+            ->selectRaw('COUNT(*) as count')
+            ->addSelect(
+                ST::distanceSphere(
+                    $origin, 'location'
+                )->as('distance')
+            )
+            ->where(
+                ST::distanceSphere($origin, 'location'), '<=', $distanceInKm * 1000
+            )
+            ->where('id', '!=', $authUser->getKey())
+            ->count();
+
+        return response()->json([
+            'count' => $count,
+            'distance' => $distanceInKm,
+            'origin' => [
+                'latitude' => $authUser->location->getLatitude(),
+                'longitude' => $authUser->location->getLongitude(),
+            ],
+        ]);
+    }
+}
---
+++ tests/Feature/Controllers/GetNearbyUserCountTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature\Controllers;
+
+use App\Models\User;
+use Clickbar\Magellan\Data\Geometries\Point;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class GetNearbyUserCountTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private const TEST_LAT_A = 43.4779751;
+    private const TEST_LNG_A = -80.5197298;
+
+    private const TEST_LAT_B = 43.470105;
+    private const TEST_LNG_B = -80.512739;
+
+    private const EXPECTED_DISTANCE = 1041.17488371; // Distance in meters
+
+    public function test_it_returns_number_of_users_nearby()
+    {
+        User::factory()->create([
+            'location' => Point::makeGeodetic(self::TEST_LAT_A, self::TEST_LNG_A),
+        ]);
+
+        User::factory()->create([
+            'location' => Point::makeGeodetic(40.0, -85.0),
+        ]);
+
+        $userTwo = User::factory()->create([
+            'location' => Point::makeGeodetic(self::TEST_LAT_B, self::TEST_LNG_B),
+        ]);
+
+        $this->actingAs($userTwo)->getJson(route('users.nearby.count', [
+            'distance' => 50,
+        ]))->assertOk()->assertJson([
+            'count' => 1,
+            'distance' => 50,
+            'origin' => [
+                'latitude' => self::TEST_LAT_B,
+                'longitude' => self::TEST_LNG_B,
+            ],
+        ]);
+    }
+}
