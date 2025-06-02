d838106f6f465c8d1deae06688b8ab877025d24c
Ryan Enns <ryanennns@gmail.com>
2025-06-02 15:57:26 -0400

    store message doc block

--- app/Http/Controllers/StoreMessageController.php
+++ app/Http/Controllers/StoreMessageController.php
@@ -8,6 +8,32 @@ use Illuminate\Http\Request;
 
 class StoreMessageController extends Controller
 {
+    /**
+     * Send a Message
+     *
+     * Stores a new message in the given chat. The authenticated user must be a participant in the chat.
+     *
+     * @authenticated
+     *
+     * @bodyParam chat_id uuid required The UUID of the chat. Example: 7d9b8e3a-1e24-4a6f-90a1-2e7b2cb80d1b
+     * @bodyParam body string required The message body. Example: Hello there
+     *
+     * @response 201 {
+     *   "message": "Message stored successfully."
+     * }
+     *
+     * @response 404 {
+     *   "message": "No query results for model [App\\Models\\Chat]..."
+     * }
+     *
+     * @response 422 {
+     *   "message": "The given data was invalid.",
+     *   "errors": {
+     *     "chat_id": ["The chat_id field is required."],
+     *     "body": ["The body field is required."]
+     *   }
+     * }
+     */
     public function __invoke(Request $request): JsonResponse
     {
         $request->validate([
