e64fd9050dedb31e53557857a8d45f6169c200e3
Ryan Enns <ryanennns@gmail.com>
2025-06-03 16:27:59 -0400

    fix channel auth

--- routes/channels.php
+++ routes/channels.php
@@ -5,11 +5,14 @@ use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\Facades\Log;
 
+Broadcast::routes(['middleware' => ['auth:sanctum']]);
+
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-Broadcast::channel('chat.{chat}', function (User $user, Chat $chat) {
-    return true;
+Broadcast::channel('chat.{chat}', function ($user, $chatId) {
+    $chat = Chat::query()->findOrFail($chatId);
+
     return $chat->user_one_id === $user->id || $chat->user_two_id === $user->id;
 });
