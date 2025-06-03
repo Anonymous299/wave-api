43a4075e6c67a54671ae3f7e88559552c24fc7ac
Ryan Enns <ryanennns@gmail.com>
2025-06-03 14:56:36 -0400

    revert

--- routes/channels.php
+++ routes/channels.php
@@ -3,11 +3,13 @@
 use App\Models\Chat;
 use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\Facades\Log;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
 Broadcast::channel('chat.{chat}', function (User $user, Chat $chat) {
+    return true;
     return $chat->user_one_id === $user->id || $chat->user_two_id === $user->id;
 });
