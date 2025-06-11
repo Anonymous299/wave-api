165681a48d7518d1832ed02c13dc10fb1cccbfe5
Ryan Enns <ryanennns@gmail.com>
2025-06-11 18:50:59 -0400

    add images to bio resource

--- app/Http/Resources/BioResource.php
+++ app/Http/Resources/BioResource.php
@@ -10,14 +10,15 @@ class BioResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            "id"         => $this->id,
-            "gender"     => $this->gender,
-            "age"        => $this->age,
-            "job"        => $this->job,
-            "company"    => $this->company,
-            "education"  => $this->education,
-            "about"      => $this->about,
-            "user_id"    => $this->user_id,
+            "id"        => $this->id,
+            "gender"    => $this->gender,
+            "age"       => $this->age,
+            "job"       => $this->job,
+            "company"   => $this->company,
+            "education" => $this->education,
+            "about"     => $this->about,
+            "user_id"   => $this->user_id,
+            "images"    => $this->images,
         ];
     }
 }
