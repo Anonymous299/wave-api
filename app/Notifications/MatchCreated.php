bb694f82875975d3167ce01b8daf2b7ced7081f2
Anonymous299 <62924701+Anonymous299@users.noreply.github.com>
2025-06-22 22:31:53 -0400

    Update MatchCreated.php

--- app/Notifications/MatchCreated.php
+++ app/Notifications/MatchCreated.php
@@ -58,6 +58,7 @@ class MatchCreated extends Notification
                     ],
                     'fcm_options' => [
                         'analytics_label' => 'match_ios',
+                        'image' => asset('images/wave_logo_400.png'),
                     ],
                 ],
             ]);
