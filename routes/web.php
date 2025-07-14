5234f82e806772ce6041bb301f4cea9ef0b24cfc
Anonymous299 <62924701+Anonymous299@users.noreply.github.com>
2025-07-14 00:09:49 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,11 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/password/reset/{token}', function ($token) {
+    return response()->json([
+        'message' => 'Use this token with your Flutter app',
+        'token' => $token,
+        'instructions' => 'Send a POST request to /api/auth/reset-password with this token, email, password, and password_confirmation'
+    ]);
+})->name('password.reset');
